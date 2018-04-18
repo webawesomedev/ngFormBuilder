9283feeb213b159da2bc994f834cfc333358e763
Ken Abe <kabe@altarepublishing.com>
2018-04-18 04:25:47 -0800

    Message Component Update

--- src/components/message.js
+++ src/components/message.js
@@ -11,10 +11,6 @@ module.exports = function(app) {
             template: 'formio/components/message/display.html'
           },
           {
-            name: 'API',
-            template: 'formio/components/common/api.html'
-          },
-          {
             name: 'Conditional',
             template: 'formio/components/common/conditional.html'
           }
@@ -33,21 +29,6 @@ module.exports = function(app) {
       // Create the settings markup.
       $templateCache.put('formio/components/message/display.html',
         '<ng-form>' +
-        '<form-builder-option property="label"></form-builder-option>' +
-        '<form-builder-option property="hideLabel"></form-builder-option>' +
-        '<form-builder-option property="customClass" label="Container Custom Class"></form-builder-option>' +
-          '<form-builder-option property="tag" label="Message Tag" placeholder="Message Element Tag" title="The tag of this Message element."></form-builder-option>' +
-          '<form-builder-option property="className" label="CSS Class" placeholder="CSS Class" title="The CSS class for this Message element."></form-builder-option>' +
-          '<value-builder ' +
-            'data="component.attrs" ' +
-            'label="Attributes" ' +
-            'tooltip-text="The attributes for this Message element. Only safe attributes are allowed, such as src, href, and title." ' +
-            'value-property="value" ' +
-            'label-property="attr" ' +
-            'value-label="Value" ' +
-            'label-label="Attribute" ' +
-            'no-autocomplete-value="true" ' +
-          '></value-builder>' +
           '<div class="form-group">' +
             '<label for="content" form-builder-tooltip="The content of this message element.">{{\'Content\' | formioTranslate}}</label>' +
             '<textarea class="form-control" id="content" name="content" ng-model="component.content" placeholder="Message Content" rows="3">{{ component.content }}</textarea>' +
